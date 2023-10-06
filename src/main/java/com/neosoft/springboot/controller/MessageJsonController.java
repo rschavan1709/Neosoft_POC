8631206260fd928417498143d30004082cd83c56
rschavan1709 <rschavan1709@gmail.com>
2023-10-06 18:39:30 +0530

    changes in rabbitmq poc

---
+++ src/main/java/com/neosoft/springboot/controller/MessageJsonController.java
@@ -0,0 +1,26 @@
+package com.neosoft.springboot.controller;
+
+import com.neosoft.springboot.dto.User;
+import com.neosoft.springboot.publisher.RabbitMQJsonProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageJsonController {
+
+    private RabbitMQJsonProducer rabbitMQJsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer rabbitMQJsonProducer) {
+        this.rabbitMQJsonProducer = rabbitMQJsonProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user){
+        rabbitMQJsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("Json message sent to RabbitMQ...");
+    }
+}
