0353da909dfddd53e3943f35bac4ec58be71fb6e
rschavan1709 <rschavan1709@gmail.com>
2023-11-17 19:01:31 +0530

    changes in pos

--- Messaging-Service/src/main/java/com/neosoft/message/controller/EmailRestController.java
+++ Messaging-Service/src/main/java/com/neosoft/message/controller/EmailRestController.java
@@ -1,26 +1,26 @@
-package com.neosoft.message.controller;
-
-import com.neosoft.message.dto.EmailSendRequest;
-import com.neosoft.message.service.EmailService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v1/mail")
-public class EmailRestController {
-
-    @Autowired
-    private EmailService emailService;
-
-    @PostMapping("/sendMail")
-    public String sendMail(@RequestBody EmailSendRequest emailSendRequest)
-    {
-        return emailService.sendMail(emailSendRequest);
-
-    }
-
-}
+//package com.neosoft.message.controller;
+//
+//import com.neosoft.message.dto.EmailSendRequest;
+//import com.neosoft.message.service.EmailService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/v1/mail")
+//public class EmailRestController {
+//
+//    @Autowired
+//    private EmailService emailService;
+//
+//    @PostMapping("/sendMail")
+//    public String sendMail(@RequestBody EmailSendRequest emailSendRequest)
+//    {
+//        return emailService.sendMail(emailSendRequest);
+//
+//    }
+//
+//}
