26074fbb44cde6e45a3f59b68d0de4d63b0412f1
rschavan1709 <rschavan1709@gmail.com>
2023-11-18 22:17:00 +0530

    changes in email functionality

--- Messaging-Service/src/main/java/com/neosoft/message/service/EmailService.java
+++ Messaging-Service/src/main/java/com/neosoft/message/service/EmailService.java
@@ -1,42 +1,42 @@
-//package com.neosoft.message.service;
-//
-//import com.neosoft.message.dto.EmailSendRequest;
-//import jakarta.mail.internet.MimeMessage;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.MimeMessageHelper;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class EmailService {
-//
-//    @Autowired
-//    private JavaMailSender javaMailSender;
-//
-//    @Value("${spring.mail.username}")
-//    private String sender;
-//
-//    public String sendMail(EmailSendRequest emailSendRequest)
-//    {
-//        try {
-//
-//            MimeMessage message = javaMailSender.createMimeMessage();
-//            MimeMessageHelper helper = new MimeMessageHelper(message);
-//
-//            helper.setFrom(sender, "Test");
-//            helper.setTo(emailSendRequest.getRecipient());
-//
-//            helper.setSubject(emailSendRequest.getSubject());
-//
-//            helper.setText(emailSendRequest.getMsgBody(), true);
-//
-//            javaMailSender.send(message);
-//            return "Mail Sent Successfully...";
-//        }
-//        catch (Exception e) {
-//            return "Error while Sending Mail";
-//        }
-//    }
-//
-//}
+package com.neosoft.message.service;
+
+import com.neosoft.message.dto.EmailSendRequest;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public String sendMail(EmailSendRequest emailSendRequest)
+    {
+        try {
+
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+
+            helper.setFrom(sender, "Test");
+            helper.setTo(emailSendRequest.getRecipient());
+
+            helper.setSubject(emailSendRequest.getSubject());
+
+            helper.setText(emailSendRequest.getMsgBody(), true);
+
+            javaMailSender.send(message);
+            return "Mail Sent Successfully...";
+        }
+        catch (Exception e) {
+            return "Error while Sending Mail";
+        }
+    }
+
+}
